d5f85d47b733a045b55c93ffd30f438cfae35403
MagicGordon <zgdzgdzgdzgd@163.com>
2021-12-18 18:14:54 +0800

    replace ref-token with test-token

--- xref-token/tests/common/init.rs
+++ xref-token/tests/common/init.rs
@@ -1,29 +1,30 @@
 use near_sdk_sim::{call, deploy, init_simulator, to_yocto, ContractAccount, UserAccount};
-use ref_token::ContractContract as RefToken;
+use test_token::ContractContract as TestToken;
 use xref_token::ContractContract as XRefToken;
 
 near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
-    REF_WASM_BYTES => "../res/ref_token_release.wasm",
-    XREF_WASM_BYTES => "../res/xref_token_local.wasm",
+    TEST_WASM_BYTES => "../res/test_token.wasm",
+    XREF_WASM_BYTES => "../res/xref_token_release.wasm",
 }
 
-pub fn init_env(register_user: bool) -> (UserAccount, UserAccount, UserAccount, ContractAccount<RefToken>, ContractAccount<XRefToken>){
+pub fn init_env(register_user: bool) -> (UserAccount, UserAccount, UserAccount, ContractAccount<TestToken>, ContractAccount<XRefToken>){
     let root = init_simulator(None);
 
     let owner = root.create_user("owner".to_string(), to_yocto("100"));
     let user = root.create_user("user".to_string(), to_yocto("100"));
 
     let ref_contract = deploy!(
-        contract: RefToken,
+        contract: TestToken,
         contract_id: "ref",
-        bytes: &REF_WASM_BYTES,
+        bytes: &TEST_WASM_BYTES,
         signer_account: root
     );
-    call!(root, ref_contract.new(owner.valid_account_id(), to_yocto("1000000").into())).assert_success();
-    call!(user, ref_contract.storage_deposit(None, None), deposit = to_yocto("1")).assert_success();
+    call!(root, ref_contract.new("ref".to_string(), "ref".to_string(), 18)).assert_success();
+    call!(owner, ref_contract.storage_deposit(None, None), deposit = to_yocto("1")).assert_success();
     call!(user, ref_contract.storage_deposit(None, None), deposit = to_yocto("1")).assert_success();
     
-    call!(owner, ref_contract.ft_transfer(user.valid_account_id(), to_yocto("100").into(), None), deposit = 1).assert_success();
+    call!(root, ref_contract.mint(owner.valid_account_id(), to_yocto("10000").into())).assert_success();
+    call!(root, ref_contract.mint(user.valid_account_id(), to_yocto("100").into())).assert_success();
 
     let xref_contract = deploy!(
         contract: XRefToken,
