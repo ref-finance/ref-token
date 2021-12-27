2bab2960f560b0526a84e4b2931c55628ffcb1ca
Marco <sun.dsk1@gmail.com>
2021-12-27 17:58:38 +0800

    fixs

--- referendum/src/proposals.rs
+++ referendum/src/proposals.rs
@@ -112,9 +112,6 @@ pub enum Action {
     VoteReject,
     /// Vote to remove given proposal or bounty (because it's spam).
     VoteRemove,
-    /// Finalize proposal, called when it's expired to return the funds
-    /// (or in the future can be used for early proposal closure).
-    Finalize,
 }
 
 impl Action {
@@ -138,7 +135,6 @@ impl From<Action> for Vote {
             Action::VoteApprove => Vote::Approve,
             Action::VoteReject => Vote::Reject,
             Action::VoteRemove => Vote::Remove,
-            _ => unreachable!(),
         }
     }
 }
